7ed6c3f10d4653094aeb473eb24e1184ff46c934
Larry Price <larry.price.dev@gmail.com>
2014-12-03 10:45:53 -0500

    omg finally getting something to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teststore::Application.config.secret_token = '68eead7f7b6177c4b56822e82e5e4bc4bb880aa9a456e831dafe1837e92c6678296825a72721418465ff2425325a3c6d102cf7355450f86e0a0c011b934db5b1'
